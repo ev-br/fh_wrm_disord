a1a0e1033493bb3a96645b7fcf0e7c76f9065d3f
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2020-10-18 15:20:31 +0300

    MAINT: generate 10 replicas for L=4

--- replicas/generate_replica.py
+++ replicas/generate_replica.py
@@ -6,11 +6,12 @@ if __name__ == "__main__":
     L = 4
     seed = 1
 
-    rndm = np.random.RandomState(seed)
-    replica = rndm.uniform(-1, 1, size=L*L*L)
+    for seed in range(1, 11):
+        rndm = np.random.RandomState(seed)
+        replica = rndm.uniform(-1, 1, size=L*L*L)
 
-    with open("disord_L%sr%s.dat" % (L, seed), "w") as f:
-        f.write(str(seed)+"\n")
-        f.write(str(L)+"\n")
-        for value in replica:
-            f.write(str(value)+"  ")
+        with open("disord_L%sr%s.dat" % (L, seed), "w") as f:
+            f.write(str(seed)+"\n")
+            f.write(str(L)+"\n")
+            for value in replica:
+                f.write(str(value)+"  ")
